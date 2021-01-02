456c676a982750ddd983392e9dab124074e568f8
tac0coder <tacojulian@outlook.com>
2021-01-02 13:02:46 -0500

    Initial Commit

---
+++ SpaceInvaders.py
@@ -0,0 +1,192 @@
+from random import choices
+import pygame
+# Start the game
+pygame.init()
+game_width = 570
+game_height = 550
+screen = pygame.display.set_mode((game_width, game_height))
+clock = pygame.time.Clock()
+running = True
+player_lives = 3
+rockets = []
+aliens = []
+# set up variables
+player_hit = pygame.image.load(
+    r'C:\Users\Growl\OneDrive\CodaKid\CodaKid_Python_1\My Classwork\space invaders\images\player_01_hit.png')
+player = pygame.image.load(
+    'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\player_01.png')
+player_rect = pygame.Rect(220, 480,
+                          player.get_width(), player.get_height())
+earth = pygame.Rect(0, 470, 570, 120)
+'''#MARK'''
+frame_counters = [45, 145, 35, 20, 50, 30]
+player_def = pygame.image.load(
+    'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\player_01.png')
+# test shoot function to test if i can shoot
+
+# setup rocket
+
+
+class Rocket():
+    def __init__(self, rocket_x, rocket_y, speed, alien):
+        # self.rocket_x = rocket_x
+        # self.rocket_y = rocket_y
+        self.speed = speed
+        self.alien = alien
+        if self.alien:
+            self.image = pygame.image.load(
+                'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\alien_laser.png')
+        else:
+            self.image = pygame.image.load(
+                'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\laser.png')
+        self.rect = pygame.Rect(rocket_x, rocket_y-self.image.get_height(),
+                                self.image.get_width(), self.image.get_height())
+
+    def shoot(self):
+        # self.image.get_rect().x = self.rocket_x
+        # self.image.get_rect().y = self.rocket_y
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        if self.alien:
+            self.rect.y += self.speed
+        else:
+            self.rect.y -= self.speed
+
+# Setup Alien
+
+
+class Alien():
+    def __init__(self, alien_x, alien_y, speed, rocket_speed):
+
+        self.speed = speed
+        self.rocket_speed = rocket_speed
+        self.image = pygame.image.load(
+            'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\alien_01.png')
+        self.rect = pygame.Rect(
+            alien_x, alien_y, self.image.get_width(), self.image.get_height())
+
+    def shoot(self):
+        if __import__('random').choice([True, False]):
+            rocket = Rocket(self.rect.x, self.rect.y,
+                            self.rocket_speed, True)
+            global rockets
+
+            rockets.append(rocket)
+
+    def move(self):
+        self.rect.x += self.speed
+        if self.rect.x >= 530:
+            self.rect.x = 0
+            self.rect.y += 60
+
+        global screen
+        if self.rect.y >= 420:
+            global player_lives
+            player_lives = 0
+
+
+# make aliens
+for i in range(0, 501, __import__('math').ceil(500/7.5)):
+    for var in range(50, 326, __import__('math').ceil(250/5.5)):
+        aliens.append(Alien(i, var, 25, 5))
+
+        # The main game loop.  This code repeats forever
+while running:
+
+    # blit lives
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(
+        0, 0, 60, player.get_height()))
+    for (i, var) in zip([20, 75, 130], list(range(__import__('math').ceil(player_lives)))):
+        screen.blit(pygame.image.load(
+            'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\player_01.png'), (i, 20))
+
+    # decrease frame counters
+    frame_counters[0] -= 1
+    frame_counters[1] -= 1
+    frame_counters[2] -= 1
+    frame_counters[3] -= 1
+    frame_counters[4] -= 1
+    frame_counters[5] -= 1
+    # draw earth
+
+    pygame.draw.rect(screen, (43, 186, 33), earth)
+    # draw player
+    screen.blit(player, (player_rect.x, player_rect.y))
+    # shoot rockets
+    for i in rockets:
+        i.shoot()
+
+    # exit test
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+    # move and shoot
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and not player_rect.x-6 <= 0:
+        player_rect.x -= 5
+
+    if keys[pygame.K_RIGHT] and not player_rect.x+5 >= 530:
+        player_rect.x += 5
+
+    if keys[pygame.K_SPACE] and frame_counters[0] <= 0:
+        rockets.append(Rocket(player_rect.x, player_rect.y-10, 3, False))
+        frame_counters[0] = 45
+    # revert player to normal image
+    if frame_counters[5] == 0:
+        player = player_def
+        frame_counters[5] = 30
+    '''#MARK Change this for faster shooting'''
+# test if an alien's shot hit the player or vice versa
+    for i in rockets:
+
+        if i.alien and player_rect.colliderect(i.rect) and frame_counters[3] <= 0:
+            player_lives -= 1
+            frame_counters[3] = 20
+            pygame.time.delay(200)
+            player = player_hit
+            del rockets[rockets.index(i)]
+        for var in aliens:
+            if not i.alien and i.rect.colliderect(var.rect) and frame_counters[4] <= 0:
+                del aliens[aliens.index(var)]
+                frame_counters[4] = 50
+                pygame.time.delay(200)
+                del rockets[rockets.index(i)]
+# test if lives are O.K.
+    if player_lives == 0:
+        screen.blit(player_hit, dest=(player_rect.x, player_rect.y))
+        pygame.time.delay(200)
+# test if aliens can shoot:
+    if frame_counters[1] <= 0:
+        try:
+            for i in choices(aliens, k=7):
+                i.shoot()
+            frame_counters[1] = 145
+        except IndexError:
+            screen.blit(pygame.image.load(
+                r'C:\Users\Growl\OneDrive\CodaKid\CodaKid_Python_1\My Classwork\space invaders\images\victory.png'), (250, (250)))
+# Blit aliens
+    for i in aliens:
+        screen.blit(i.image, (i.rect.x, i.rect.y))
+    if frame_counters[2] <= 0:
+        frame_counters[2] = 35
+        for i in aliens:
+            i.move()
+    # test if lives are ok
+    if player_lives <= 0:
+        running = False
+
+    pygame.display.flip()
+    clock.tick(50)
+    screen.fill((0, 0, 0))
+while not running and player_lives == 0:
+    screen.blit(pygame.image.load(
+        'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\defeat.png'), (220, 250))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = True
+    pygame.display.flip()
+    clock.tick(50)
+    screen.fill((0, 0, 0))
---
+++ SpaceInvadersMinify.py
@@ -0,0 +1,145 @@
+import pygame as A
+from random import choices as V
+U = 'math'
+T = 'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\player_01.png'
+S = range
+N = True
+M = False
+A.init()
+W = 570
+X = 550
+D = A.display.set_mode((W, X))
+P = A.time.Clock()
+F = N
+K = 3
+G = []
+H = []
+Q = A.image.load(
+    'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\player_01_hit.png')
+J = A.image.load(T)
+E = A.Rect(220, 480, J.get_width(), J.get_height())
+Y = A.Rect(0, 470, 570, 120)
+C = [45, 145, 35, 20, 50, 30]
+Z = A.image.load(T)
+
+
+class R:
+    def __init__(B, rocket_x, rocket_y, speed, alien):
+        B.speed = speed
+        B.alien = alien
+        if B.alien:
+            B.image = A.image.load(
+                'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\alien_laser.png')
+        else:
+            B.image = A.image.load(
+                'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\laser.png')
+        B.rect = A.Rect(rocket_x, rocket_y-B.image.get_height(),
+                        B.image.get_width(), B.image.get_height())
+
+    def shoot(A):
+        D.blit(A.image, (A.rect.x, A.rect.y))
+        if A.alien:
+            A.rect.y += A.speed
+        else:
+            A.rect.y -= A.speed
+
+
+class a:
+    def __init__(B, alien_x, alien_y, speed, rocket_speed): B.speed = speed; B.rocket_speed = rocket_speed; B.image = A.image.load(
+        'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\alien_01.png'); B.rect = A.Rect(alien_x, alien_y, B.image.get_width(), B.image.get_height())
+
+    def shoot(A):
+        if __import__('random').choice([N, M]):
+            B = R(A.rect.x, A.rect.y, A.rocket_speed, N)
+            global G
+            G.append(B)
+
+    def move(A):
+        A.rect.x += A.speed
+        if A.rect.x >= 530:
+            A.rect.x = 0
+            A.rect.y += 60
+        global K
+        if A.rect.y >= 420:
+            K = 0
+
+
+for B in S(0, 501, __import__(U).ceil(500/7.5)):
+    for L in S(50, 326, __import__(U).ceil(250/5.5)):
+        H.append(a(B, L, 25, 5))
+while F:
+    A.draw.rect(D, (0, 0, 0), A.Rect(0, 0, 60, J.get_height()))
+    for (B, L) in zip([20, 75, 130], list(S(__import__(U).ceil(K)))):
+        D.blit(A.image.load(T), (B, 20))
+    C[0] -= 1
+    C[1] -= 1
+    C[2] -= 1
+    C[3] -= 1
+    C[4] -= 1
+    C[5] -= 1
+    A.draw.rect(D, (43, 186, 33), Y)
+    D.blit(J, (E.x, E.y))
+    for B in G:
+        B.shoot()
+    for I in A.event.get():
+        if I.type == A.QUIT:
+            F = M
+        if I.type == A.KEYDOWN and I.key == A.K_ESCAPE:
+            F = M
+    O = A.key.get_pressed()
+    if O[A.K_LEFT] and not E.x-6 <= 0:
+        E.x -= 5
+    if O[A.K_RIGHT] and not E.x+5 >= 530:
+        E.x += 5
+    if O[A.K_SPACE] and C[0] <= 0:
+        G.append(R(E.x, E.y-10, 3, M))
+        C[0] = 45
+    if C[5] == 0:
+        J = Z
+        C[5] = 30
+    for B in G:
+        if B.alien and E.colliderect(B.rect) and C[3] <= 0:
+            K -= 1
+            C[3] = 20
+            A.time.delay(200)
+            J = Q
+            del G[G.index(B)]
+        for L in H:
+            if not B.alien and B.rect.colliderect(L.rect) and C[4] <= 0:
+                del H[H.index(L)]
+                C[4] = 50
+                A.time.delay(200)
+                del G[G.index(B)]
+    if K == 0:
+        D.blit(Q, dest=(E.x, E.y))
+        A.time.delay(200)
+    if C[1] <= 0:
+        try:
+            for B in V(H, k=7):
+                B.shoot()
+            C[1] = 145
+        except IndexError:
+            D.blit(A.image.load(
+                'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\victory.png'), (250, 250))
+    for B in H:
+        D.blit(B.image, (B.rect.x, B.rect.y))
+    if C[2] <= 0:
+        C[2] = 35
+        for B in H:
+            B.move()
+    if K <= 0:
+        F = M
+    A.display.flip()
+    P.tick(50)
+    D.fill((0, 0, 0))
+while not F and K == 0:
+    D.blit(A.image.load(
+        'C:\\Users\\Growl\\OneDrive\\CodaKid\\CodaKid_Python_1\\My Classwork\\space invaders\\images\\defeat.png'), (220, 250))
+    for I in A.event.get():
+        if I.type == A.QUIT:
+            F = N
+        if I.type == A.KEYDOWN and I.key == A.K_ESCAPE:
+            F = N
+    A.display.flip()
+    P.tick(50)
+    D.fill((0, 0, 0))
